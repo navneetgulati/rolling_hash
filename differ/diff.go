e04fe2194b46aa1644fe9b52ba46fe929179c390
gulatinavneet <navneet.gulati@block8.com>
2023-02-11 13:33:22 +0530

    Fix overflowing subtraction during roll out

--- differ/diff.go
+++ differ/diff.go
@@ -5,115 +5,132 @@ import (
 	"io"
 
 	"github.com/gulatinavneet/rollingHash/hasher"
+	"github.com/gulatinavneet/rollingHash/utils"
 )
 
-type Delta struct{
-	startIndex int
-	endIndex int
-	deleted bool
+type Delta struct {
+	startIndex      int
+	endIndex        int
+	deleted         bool
+	updatedLiterals []byte
+}
+type PrettyDelta struct {
+	startIndex      int
+	endIndex        int
+	deleted         bool
 	updatedLiterals string
 }
-
-type Differ struct{
+type Differ struct {
 	chunkSize int //ChunkSize in bytes
 }
 
-func New(chunkSize int) *Differ{
-	return &Differ{ chunkSize: chunkSize}
+func New(chunkSize int) *Differ {
+	return &Differ{chunkSize: chunkSize}
 }
 
-func (diff *Differ)GenerateSignatures(reader *bufio.Reader) map[uint]int{
-	var signatures = make(map[uint]int)
-	var count=0
+func (d *Differ) GenerateSignatures(reader *bufio.Reader) map[uint]int {
+	signatures := make(map[uint]int)
+	count := 0
 	for {
-		chunk := make([]byte,diff.chunkSize)
-		bytes,err:=reader.Read(chunk)
-		if bytes ==0 || err == io.EOF{
+		chunk := make([]byte, d.chunkSize)
+		bytes, err := reader.Read(chunk)
+		if bytes == 0 || err == io.EOF {
 			break
 		}
-		if bytes<diff.chunkSize{
-			chunk=chunk[:bytes]
+		if bytes < d.chunkSize {
+			chunk = chunk[:bytes]
 		}
-		adler32:= hasher.NewAdler32(diff.chunkSize)
+		adler32 := hasher.NewAdler32(d.chunkSize)
 		adler32.Write(chunk)
-		signature:= adler32.Hash()
-		signatures[signature]=count
+		signature := adler32.Hash()
+		signatures[signature] = count
 		count++
 	}
 	return signatures
 }
 
-func (diff *Differ) GenerateDelta(signatures map[uint]int,reader *bufio.Reader) map[int]Delta{
-	deltas:= make(map[int]Delta)
-	adler32 := hasher.NewAdler32(diff.chunkSize)
+func (d *Differ) GenerateDelta(signatures map[uint]int, reader *bufio.Reader) map[int]Delta {
+	deltas := make(map[int]Delta)
+	adler32 := hasher.NewAdler32(d.chunkSize)
 
 	var diffingLiterals []byte
-	lastFoundIndex:=-1
+	lastFoundIndex := -1
 
 	//Write logic for finding it in existing signature file
-	for{
-		c,err:= reader.ReadByte()
-		if err == io.EOF || err!=nil {
+	for {
+		c, err := reader.ReadByte()
+		if err == io.EOF || err != nil {
 			break
 		}
-		hash:=adler32.RollIn(c)
-
-		if adler32.WindowLength()<diff.chunkSize{
+		hash := adler32.RollIn(c)
+		if adler32.WindowLength() < d.chunkSize {
 			//Check if this is not the last byte before continuing
-			if next,_:=reader.Peek(1);len(next)>0{
+			if next, _ := reader.Peek(1); len(next) > 0 {
 				continue
 			}
 		}
-		index := findIndex(signatures,hash)
+		index := findIndex(signatures, hash)
 
-		if index != -1{
+		if index != -1 {
 			adler32.Reset()
-			deltas[index]=Delta{
-				startIndex: index*diff.chunkSize+1,
-				endIndex: (index+1)*diff.chunkSize,
-				deleted: false,
-				updatedLiterals: string(diffingLiterals),
+			deltas[index] = Delta{
+				startIndex:      (index-1)*d.chunkSize + 1,
+				endIndex:        (index) * d.chunkSize,
+				deleted:         false,
+				updatedLiterals: diffingLiterals,
 			}
-			diffingLiterals=diffingLiterals[:0]
-			lastFoundIndex=index
+			utils.Clear(&diffingLiterals)
+			lastFoundIndex = index
 			continue
 		}
-			_,removed:=adler32.RollOut()
-			diffingLiterals = append(diffingLiterals, removed)
+		_, removed := adler32.RollOut()
+		diffingLiterals = append(diffingLiterals, removed)
 	}
-	//If 
-	if len(diffingLiterals)>0{
+	if len(diffingLiterals) > 0 {
 		diffingLiterals = append(diffingLiterals, adler32.GetWindowLiterals()...)
-		deltas[lastFoundIndex+1]=Delta{
-			startIndex: (lastFoundIndex+1)*diff.chunkSize,
-			endIndex: (lastFoundIndex+1)*diff.chunkSize+len(diffingLiterals),
-			deleted: false,
-			updatedLiterals: string(diffingLiterals),
+		deltas[lastFoundIndex+1] = Delta{
+			startIndex:      (lastFoundIndex + 1) * d.chunkSize,
+			endIndex:        (lastFoundIndex + 2) * d.chunkSize,
+			deleted:         false,
+			updatedLiterals: diffingLiterals,
 		}
 	}
-	deltas=diff.updateDeletedChunks(signatures,deltas)
+	deltas = d.updateDeletedChunks(signatures, deltas)
 	return deltas
 }
 
-func (diff *Differ)updateDeletedChunks(signatures map[uint]int, delta map[int]Delta)map[int]Delta{
-	for _,index:=range signatures{
-		if value,ok:=delta[index];!ok{
-			delta[index]=Delta{
-				startIndex: index*diff.chunkSize+1,
-				endIndex: (index+1)*diff.chunkSize,
-				deleted: true,
-				updatedLiterals: "",
+func (d *Differ) updateDeletedChunks(signatures map[uint]int, delta map[int]Delta) map[int]Delta {
+	for _, index := range signatures {
+		if value, ok := delta[index]; !ok {
+			delta[index] = Delta{
+				startIndex:      (index)*d.chunkSize + 1,
+				endIndex:        (index + 1) * d.chunkSize,
+				deleted:         true,
+				updatedLiterals: []byte{},
 			}
-		}else if len(value.updatedLiterals)==0{
-			delete(delta,index)
+		} else if len(value.updatedLiterals) == 0 {
+			delete(delta, index)
 		}
 	}
 	return delta
 }
 
-func findIndex(signatures map[uint]int,hash uint) int{
-	if index,found:= signatures[hash];found{
+func findIndex(signatures map[uint]int, hash uint) int {
+	if index, found := signatures[hash]; found {
 		return index
 	}
 	return -1
+}
+
+func PrettifyDelta(deltas map[int]Delta) map[int]PrettyDelta {
+	prettyDelta := make(map[int]PrettyDelta)
+	for key, value := range deltas {
+		prettyDelta[key] = PrettyDelta{
+			startIndex:      value.startIndex,
+			endIndex:        value.endIndex,
+			deleted:         value.deleted,
+			updatedLiterals: string(value.updatedLiterals),
+		}
+	}
+	return prettyDelta
 }
